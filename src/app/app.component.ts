5934c8712c4ee9bff60c178f10b60317bcb913cb
jenniferzrd <jennifer.zarid@gmail.com>
2017-08-31 09:52:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'hello voici ma premiere modif sous angular';
+  maData = "";
 }
